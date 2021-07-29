662fbe3960a896b1a9330f9ee4705b2aa4bd405a
leila <leila.boubaker20@gmail.com>
2021-07-29 12:38:13 +0100

    component

---
+++ src/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+    return (
+        <div className= 'footer'>
+          CopyRigthe f1  
+        </div>
+    )
+}
+
+export default Footer
